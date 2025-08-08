83846606a06553fef72994027721cbb9cdc5b31d
Simon Dickson <hello@simondickson.co.uk>
2025-08-08 14:20:03 +0100

    Adds extra data. Tries encrypting server info.

--- inc/datadump.php
+++ inc/datadump.php
@@ -14,20 +14,43 @@ add_action('wp_footer', function() {
         "queryVars" => $wp_query->query_vars,
         "bodyClasses" => get_body_class(),
     );
+
+    $user_data = array(
+        "id" => $user->id,
+        "displayName" => $user->display_name
+    );
+
+    // Combine all data into a single object
+    $alpaca_data = array(
+        "time" => time(),
+        "user" => $user_data,
+        "server" => $_SERVER,
+        "wp" => $wp_data,
+    );
+
+    // Encode the JSON string into Base64:
+    // $encoded_data = base64_encode(json_encode($alpaca_data));
+    // Then to decode the Base64 string and parse the JSON:
+    // in JS: decoded_data=atob(encoded_data) then JSON.parse(decoded_data);
+    // in PHP: $decoded_data=base64_decode($encoded_string) then json_decode($decoded_string)
+
+    $alpaca_json = json_encode($alpaca_data);
+
 ?>
 <script type="text/javascript">
     const b = bowser.parse(window.navigator.userAgent);
+
     const alpaca_data = {
-        user: { id: <?php echo $user->id; ?>, displayName: <?php echo json_encode($user->display_name); ?> },
-        device: {
-            browser: { name: b.browser.name, version: b.browser.version, },
-			vendor: b.platform.vendor,
-            type: b.platform.type,
-            os: b.os.name,
-            version: b.os.version,
-            versionName: b.os.versionName
-        },
-        wp: <?php echo json_encode($wp_data) ?>
+        "encoded": "<?php echo base64_encode($alpaca_json); ?>",
+        "unencoded": <?php echo $alpaca_json; ?>,
+        "device": {
+            "browser": { name: b.browser.name, version: b.browser.version, },
+            "vendor": b.platform.vendor,
+            "type": b.platform.type,
+            "os": b.os.name,
+            "version": b.os.version,
+            "versionName": b.os.versionName
+        }
     };
 </script>
 <?php
