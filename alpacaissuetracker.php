b0efc78586a014abe428bf0d5cdfd13db5cacb31
Pratik Barvaliya <barvaliyapratik36@gmail.com>
2026-06-05 15:48:53 -0400

    chore(release): release 1.0.3

--- alpacaissuetracker.php
+++ alpacaissuetracker.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Alpaca Issue Tracker
  * Plugin URI:        https://github.com/rareview/alpaca-issue-tracker
  * Description:       A lightweight issue tracker built entirely inside WordPress, designed for developers and agencies managing client projects.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Requires at least: 6.8
  * Requires PHP:      7.4
  * Author:            Rareview®
